ee870b32941777c84a201e3b0bc3e97839773ad6
Aparna Gorthi <aparnagorthi@192.168.1.6>
2026-06-26 13:49:23 +0530

    fix startup command

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pip install uvicorn fastapi sqlalchemy psycopg2-binary python-dotenv python-jose passlib bcrypt python-multipart
-uvicorn main:app --host 0.0.0.0 --port 8000
+pip install -r requirements.txt
+python -m uvicorn main:app --host 0.0.0.0 --port 8000
