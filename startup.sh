9d8e6f4c241db8a1792d3949279a739cc3b37780
Aparna Gorthi <aparnagorthi@192.168.1.6>
2026-06-26 13:24:52 +0530

    fix: use full path in startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-pip install -r requirements.txt
+pip install -r /home/site/wwwroot/requirements.txt
+cd /home/site/wwwroot
 gunicorn main:app --workers 2 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --timeout 120
