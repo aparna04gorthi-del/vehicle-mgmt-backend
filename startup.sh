7343ec1651f97d764df6e8fcba55364ef109c73d
Aparna Gorthi <aparnagorthi@192.168.1.6>
2026-06-26 12:57:46 +0530

    fix: add requirements, switch to gunicorn

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip install -r requirements.txt
-uvicorn main:app --host 0.0.0.0 --port 8000
+gunicorn main:app --workers 2 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --timeout 120
